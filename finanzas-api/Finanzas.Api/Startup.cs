4299fd81a069ea32adad211d502e5722ac2075db
Jhohan Alexander Vasquez Sierra <Jhohan_Vasquez@satrack.com>
2025-08-24 23:57:46 -0500

    Se realizan ajustes a formualrios y estilos

--- finanzas-api/Finanzas.Api/Startup.cs
+++ finanzas-api/Finanzas.Api/Startup.cs
@@ -54,6 +54,7 @@ namespace Finanzas.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("PermitirTodo");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
